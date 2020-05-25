3fa36b8631c9c3ce12000644a10936f2cd2fd9fe
Nuri Mertcan Guner <NMGuner@users.noreply.github.com>
2020-05-25 15:27:35 +0200

    add field integration for authorizationRequest and renderOperation

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -10,77 +10,154 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  final localeController = TextEditingController();
-  final countryController = TextEditingController();
+  final localeController = TextEditingController(text: "sv-SE");
+  final countryController = TextEditingController(text: "SE");
+
+  final clientIdController = TextEditingController(text: "");
+  final scopeController = TextEditingController(text: "read:consumer_order");
+  final redirectUriController = TextEditingController(text: "");
+
+  final operationTokenController = TextEditingController(text: "");
 
   @override
   void initState() {
     super.initState();
     initHybridSDK();
-
-    localeController.text = "sv-SE";
-    countryController.text = "SE";
   }
 
   void initHybridSDK() async {
     await KlarnaHybridSDK.initialize("");
   }
 
-  void initPostPurchaseExperience() async {
+  void ppeInitialize() async {
     await KlarnaPostPurchaseExperience.initialize(
         localeController.text, countryController.text,
         design: null);
+  }
+
+  void ppeAuthorizationRequest() async {
+    await KlarnaPostPurchaseExperience.authorizationRequest(
+        clientIdController.text,
+        scopeController.text,
+        redirectUriController.text);
+  }
+
+  void ppeRenderOperation() async {
+    await KlarnaPostPurchaseExperience.renderOperation(
+        operationTokenController.text);
   }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Flutter Klarna In-App SDK Example'),
-        ),
-        body: Stack(
-          children: [
-            Positioned.fill(
-              child: DecoratedBox(
-                decoration: BoxDecoration(
-                  color: Theme.of(context).scaffoldBackgroundColor,
+          appBar: AppBar(
+            title: const Text('Flutter Klarna In-App SDK Example'),
+          ),
+          body: SingleChildScrollView(
+            child: Stack(
+              children: [
+                Positioned.fill(
+                  child: DecoratedBox(
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).scaffoldBackgroundColor,
+                    ),
+                  ),
                 ),
-              ),
-            ),
-            Positioned.fill(
-              child: Opacity(
-                opacity: .25,
-                child: FittedBox(
-                  fit: BoxFit.cover,
-                  child: FlutterLogo(),
+                Positioned.fill(
+                  child: Opacity(
+                    opacity: .25,
+                    child: FittedBox(
+                      fit: BoxFit.cover,
+                      child: FlutterLogo(),
+                    ),
+                  ),
                 ),
-              ),
+                Container(
+                  child: Column(children: [
+                    Container(
+                      child: Column(
+                        children: <Widget>[
+                          Text(
+                            "PostPurchaseExperience.initialize",
+                            style: Theme.of(context).textTheme.subtitle,
+                          ),
+                          TextFormField(
+                            controller: localeController,
+                            decoration: InputDecoration(
+                                border: InputBorder.none, labelText: "Locale"),
+                          ),
+                          TextFormField(
+                            controller: countryController,
+                            decoration: InputDecoration(
+                                border: InputBorder.none, labelText: 'Country'),
+                          ),
+                          MaterialButton(
+                            color: Theme.of(context).accentColor,
+                            child: new Text("Initialize"),
+                            onPressed: ppeInitialize,
+                          ),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      child: Column(
+                        children: <Widget>[
+                          Text(
+                            "PostPurchaseExperience.authorizationRequest",
+                            style: Theme.of(context).textTheme.subtitle,
+                          ),
+                          TextFormField(
+                            controller: clientIdController,
+                            decoration: InputDecoration(
+                                border: InputBorder.none,
+                                labelText: "Client ID"),
+                          ),
+                          TextFormField(
+                            controller: scopeController,
+                            decoration: InputDecoration(
+                                border: InputBorder.none, labelText: 'Scope'),
+                          ),
+                          TextFormField(
+                            controller: redirectUriController,
+                            decoration: InputDecoration(
+                                border: InputBorder.none,
+                                labelText: 'Redirect URI'),
+                          ),
+                          MaterialButton(
+                            color: Theme.of(context).accentColor,
+                            child: new Text("Authorization Request"),
+                            onPressed: ppeAuthorizationRequest,
+                          ),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      child: Column(
+                        children: <Widget>[
+                          Text(
+                            "PostPurchaseExperience.renderOperation",
+                            style: Theme.of(context).textTheme.subtitle,
+                          ),
+                          TextFormField(
+                            controller: operationTokenController,
+                            decoration: InputDecoration(
+                                border: InputBorder.none,
+                                labelText: "Operation Token"),
+                          ),
+                          MaterialButton(
+                            color: Theme.of(context).accentColor,
+                            child: new Text("Render Operation"),
+                            onPressed: ppeAuthorizationRequest,
+                          ),
+                        ],
+                      ),
+                    )
+                  ]),
+                )
+              ],
             ),
-            Container(
-              child: Column(
-                children: <Widget>[
-                  TextFormField(
-                    controller: localeController,
-                    decoration: InputDecoration(
-                        border: InputBorder.none, labelText: "Locale"),
-                  ),
-                  TextFormField(
-                    controller: countryController,
-                    decoration: InputDecoration(
-                        border: InputBorder.none, labelText: 'Country'),
-                  ),
-                  MaterialButton(
-                    color: Theme.of(context).primaryColor,
-                    child: new Text("Initialize PostPurchaseExperience"),
-                    onPressed: initPostPurchaseExperience,
-                  ),
-                ],
-              ),
-            )
-          ],
-        ),
-      ),
+          )),
     );
   }
 }
