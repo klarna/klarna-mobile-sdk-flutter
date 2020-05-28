d572b6cfafefe7f51aba8a12adb09ae95ec3ad15
Nuri Mertcan Guner <NMGuner@users.noreply.github.com>
2020-05-28 10:36:20 +0200

    add event listener impl

--- ios/Classes/Hybrid/PluginKlarnaHybridEventListener.swift
+++ ios/Classes/Hybrid/PluginKlarnaHybridEventListener.swift
@@ -1,24 +1,27 @@
 import KlarnaMobileSDK
 
 class PluginKlarnaHybridEventListener: KlarnaHybridEventListener {
+    
+    var result: FlutterResult?
+    
     func klarnaWillShowFullscreen(inWebView webView: KlarnaWebView, completionHandler: @escaping () -> Void) {
-        
+        completionHandler()
     }
     
     func klarnaDidShowFullscreen(inWebView webView: KlarnaWebView, completionHandler: @escaping () -> Void) {
-        
+        completionHandler()
     }
     
     func klarnaWillHideFullscreen(inWebView webView: KlarnaWebView, completionHandler: @escaping () -> Void) {
-        
+        completionHandler()
     }
     
     func klarnaDidHideFullscreen(inWebView webView: KlarnaWebView, completionHandler: @escaping () -> Void) {
-        
+        completionHandler()
     }
     
     func klarnaFailed(inWebView webView: KlarnaWebView, withError error: KlarnaMobileSDKError) {
-        
+        result?(FlutterError.init(code: ResultError.hybridSdkError.rawValue, message: "\(error.name):\(error.message):\(error.isFatal)", details: error.message))
     }
     
     
