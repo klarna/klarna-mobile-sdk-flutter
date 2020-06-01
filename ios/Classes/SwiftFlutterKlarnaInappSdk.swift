7720bd57c7c430445dfaf59853d26cc31f5aee74
Nuri Mertcan Guner <NMGuner@users.noreply.github.com>
2020-06-01 12:05:29 +0200

    add event and error stream handlers from android

--- ios/Classes/SwiftFlutterKlarnaInappSdk.swift
+++ ios/Classes/SwiftFlutterKlarnaInappSdk.swift
@@ -5,16 +5,28 @@ public class SwiftFlutterKlarnaInappSdk: NSObject, FlutterPlugin {
     public static func register(with registrar: FlutterPluginRegistrar) {
         let messenger = registrar.messenger()
         
-        getHandlerMap().forEach { (key, value) in
+        getMethodHandlerMap().forEach { (key, value) in
             let channel = FlutterMethodChannel(name: key, binaryMessenger: messenger)
             registrar.addMethodCallDelegate(value, channel: channel)
         }
+        
+        getStreamHandlerMap().forEach { (key, value) in
+            let channel = FlutterEventChannel(name: key, binaryMessenger: messenger)
+            channel.setStreamHandler(value)
+        }
     }
     
-    static func getHandlerMap() -> [String: FlutterPlugin] {
+    static func getMethodHandlerMap() -> [String: FlutterPlugin] {
         return [
             "klarna_hybrid_sdk": KlarnaHybridSDKHandler(),
             "klarna_post_purchase_experience": PostPurchaseHandler()
+        ]
+    }
+    
+    static func getStreamHandlerMap() -> [String: FlutterStreamHandler & NSObjectProtocol] {
+        return [
+            "klarna_events": EventCallbackHandler.instance,
+            "klarna_errors": ErrorCallbackHandler.instance
         ]
     }
 }
