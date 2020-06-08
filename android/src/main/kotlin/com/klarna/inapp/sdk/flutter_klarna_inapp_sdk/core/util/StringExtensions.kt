a93dd49ef29c8857fe30ad1f5a7b90522eb8ffa5
Nuri Mertcan Guner <NMGuner@users.noreply.github.com>
2020-06-08 11:24:32 +0200

    fix js string extension

--- android/src/main/kotlin/com/klarna/inapp/sdk/flutter_klarna_inapp_sdk/core/util/StringExtensions.kt
+++ android/src/main/kotlin/com/klarna/inapp/sdk/flutter_klarna_inapp_sdk/core/util/StringExtensions.kt
@@ -2,7 +2,7 @@ package com.klarna.inapp.sdk.flutter_klarna_inapp_sdk.core.util
 
 internal fun String?.jsScriptString(): String {
     return if (this != null) {
-        "'$this'"
+        "\"${this.replace("\"", "\\\"", false)}\""
     } else {
         "null"
     }
