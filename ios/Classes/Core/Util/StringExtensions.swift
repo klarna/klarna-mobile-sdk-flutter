368ebd6322593b193ea974f58c2edcc182f5f37b
Nuri Mertcan Guner <NMGuner@users.noreply.github.com>
2020-06-08 10:50:45 +0200

    fix js string extension

--- ios/Classes/Core/Util/StringExtensions.swift
+++ ios/Classes/Core/Util/StringExtensions.swift
@@ -4,14 +4,14 @@ extension String: Error {}
 
 extension String {
     func jsScriptString() -> String {
-        return "\"\(self)\""
+        return "\"\(self.replacingOccurrences(of: "\"", with: "\\\""))\""
     }
 }
 
 extension Optional where Wrapped == String {
     func jsScriptString() -> String {
         if let it = self {
-            return "\"\(it)\""
+            return it.jsScriptString()
         } else {
             return "null"
         }
