34738c12570bb228611123b3fc7083a98614176f
Nuri Mertcan Guner <NMGuner@users.noreply.github.com>
2020-06-12 14:22:27 +0200

    add instance support to android

---
+++ android/src/main/kotlin/com/klarna/inapp/sdk/flutter_klarna_inapp_sdk/postpurchase/PostPurchaseExperienceManager.kt
@@ -0,0 +1,140 @@
+package com.klarna.inapp.sdk.flutter_klarna_inapp_sdk.postpurchase
+
+import android.view.View
+import android.webkit.WebChromeClient
+import com.klarna.inapp.sdk.flutter_klarna_inapp_sdk.ErrorCallbackHandler
+import com.klarna.inapp.sdk.flutter_klarna_inapp_sdk.ResultError
+import com.klarna.inapp.sdk.flutter_klarna_inapp_sdk.core.util.jsScriptString
+import com.klarna.inapp.sdk.flutter_klarna_inapp_sdk.core.webview.WebViewManager
+import com.klarna.inapp.sdk.flutter_klarna_inapp_sdk.hybrid.KlarnaHybridSDKHandler
+import io.flutter.plugin.common.MethodChannel
+
+internal class PostPurchaseExperienceManager {
+
+    companion object {
+        private const val NOT_INITIALIZED = "PostPurchaseExperience is not initialized"
+
+        private fun notInitialized(result: MethodChannel.Result?) {
+            result?.error(
+                    ResultError.POST_PURCHASE_ERROR.errorCode,
+                    NOT_INITIALIZED,
+                    "Call 'PostPurchaseExperience.initialize' before this."
+            )
+        }
+    }
+
+    private var webViewManager: WebViewManager = WebViewManager()
+
+    private var webViewClient: PostPurchaseExperienceWebViewClient? = null
+
+    private var initialized = false
+
+    private var initResult: MethodChannel.Result? = null
+    private var authResult: MethodChannel.Result? = null
+    private var renderResult: MethodChannel.Result? = null
+
+    private val jsInterface = PostPurchaseExperienceJSInterface(PPEResultCallback())
+
+    fun initialize(method: PostPurchaseExperienceMethod.Initialize, result: MethodChannel.Result?) {
+        if (webViewManager.webView != null || initialized) {
+            result?.error(ResultError.POST_PURCHASE_ERROR.errorCode, "Already initialized.", null)
+            return
+        }
+
+        val hybridSDK = KlarnaHybridSDKHandler.hybridSDK
+        if (hybridSDK != null) {
+            webViewClient = PostPurchaseExperienceWebViewClient(hybridSDK)
+        } else {
+            KlarnaHybridSDKHandler.notInitialized(result)
+            return
+        }
+
+        webViewManager.initialize(null)
+        webViewManager.addToHybridSdk(null)
+
+        val webView = webViewManager.requireWebView()
+        webView.webChromeClient = WebChromeClient()
+        webView.webViewClient = webViewClient
+        webView.settings.javaScriptEnabled = true
+        webView.settings.domStorageEnabled = true
+        webView.visibility = View.INVISIBLE
+
+        webView.addJavascriptInterface(jsInterface, "PPECallback")
+        webView.loadUrl("file:///android_asset/ppe.html")
+
+        val initScript = "initialize(${method.locale.jsScriptString()}, ${method.purchaseCountry.jsScriptString()}, ${method.design.jsScriptString()})"
+        webViewClient?.queueJS(webViewManager, initScript)
+
+        initResult = result
+    }
+
+    fun destroy(method: PostPurchaseExperienceMethod.Destroy, result: MethodChannel.Result?) {
+        webViewManager.destroy(null)
+        webViewManager = WebViewManager()
+        webViewClient = null
+        initialized = false
+        initResult = null
+        renderResult = null
+        authResult = null
+        result?.success(null)
+    }
+
+    fun renderOperation(method: PostPurchaseExperienceMethod.RenderOperation, result: MethodChannel.Result?) {
+        if (!initialized) {
+            notInitialized(result)
+            return
+        }
+        renderResult = result
+        webViewClient?.queueJS(webViewManager, "renderOperation(${method.locale.jsScriptString()}, ${method.operationToken.jsScriptString()})")
+    }
+
+    fun authorizationRequest(method: PostPurchaseExperienceMethod.AuthorizationRequest, result: MethodChannel.Result?) {
+        if (!initialized) {
+            notInitialized(result)
+            return
+        }
+        authResult = result
+        webViewClient?.queueJS(webViewManager, "authorizationRequest(${method.locale.jsScriptString()}, " +
+                "${method.clientId.jsScriptString()}, " +
+                "${method.scope.jsScriptString()}, " +
+                "${method.redirectUri.jsScriptString()}, " +
+                "${method.state.jsScriptString()}, " +
+                "${method.loginHint.jsScriptString()}, " +
+                "${method.responseType.jsScriptString()})")
+    }
+
+    inner class PPEResultCallback : PostPurchaseExperienceJSInterface.ResultCallback {
+
+        override fun onInitialized(success: Boolean, error: String?) {
+            if (success) {
+                initResult?.success(null)
+                initialized = true
+            } else {
+                initResult?.error(ResultError.POST_PURCHASE_ERROR.errorCode, error, null)
+            }
+            initResult = null
+        }
+
+        override fun onRenderOperation(success: Boolean, data: String?, error: String?) {
+            if (success) {
+                renderResult?.success(data)
+            } else {
+                renderResult?.error(ResultError.POST_PURCHASE_ERROR.errorCode, error, data)
+            }
+            renderResult = null
+        }
+
+        override fun onAuthorizationRequest(success: Boolean, error: String?) {
+            if (success) {
+                authResult?.success(null)
+            } else {
+                authResult?.error(ResultError.POST_PURCHASE_ERROR.errorCode, error, null)
+            }
+            authResult = null
+        }
+
+        override fun onError(message: String?, throwable: Throwable?) {
+            ErrorCallbackHandler.sendValue(message)
+        }
+    }
+}
--- lib/klarna_post_purchase_experience.dart
+++ lib/klarna_post_purchase_experience.dart
@@ -1,32 +1,43 @@
 import 'dart:async';
+import 'dart:math';
 
 import 'package:flutter/services.dart';
 
 class KlarnaPostPurchaseExperience {
-
   static const MethodChannel _channel =
       const MethodChannel('klarna_post_purchase_experience');
 
-  static Future<void> initialize(String locale, String purchaseCountry,
+  static final idRandom = Random.secure();
+
+  var id = idRandom.nextInt(999999999);
+
+  KlarnaPostPurchaseExperience._();
+
+  static Future<KlarnaPostPurchaseExperience> initialize(String locale, String purchaseCountry,
       {String design}) async {
-    return await _channel.invokeMethod('initialize', <String, dynamic>{
+    var instance = KlarnaPostPurchaseExperience._();
+    await _channel.invokeMethod('initialize', <String, dynamic>{
+      'id': instance.id,
       'locale': locale,
       'purchaseCountry': purchaseCountry,
       'design': design
     });
+    return instance;
   }
 
-  static Future<void> destroy() async {
-    return await _channel.invokeMethod('destroy');
+  Future<void> destroy() async {
+    return await _channel.invokeMethod('destroy', <String, dynamic>{'id': id});
   }
 
-  static Future<String> renderOperation(String operationToken,
-      {String locale}) async {
-    return await _channel.invokeMethod('renderOperation',
-        <String, dynamic>{'locale': locale, 'operationToken': operationToken});
+  Future<String> renderOperation(String operationToken, {String locale}) async {
+    return await _channel.invokeMethod('renderOperation', <String, dynamic>{
+      'id': id,
+      'locale': locale,
+      'operationToken': operationToken
+    });
   }
 
-  static Future<void> authorizationRequest(
+  Future<void> authorizationRequest(
       String clientId, String scope, String redirectUri,
       {String locale,
       String state,
@@ -34,6 +45,7 @@ class KlarnaPostPurchaseExperience {
       String responseType}) async {
     return await _channel
         .invokeMethod('authorizationRequest', <String, dynamic>{
+      'id': id,
       'locale': locale,
       'clientId': clientId,
       'scope': scope,
