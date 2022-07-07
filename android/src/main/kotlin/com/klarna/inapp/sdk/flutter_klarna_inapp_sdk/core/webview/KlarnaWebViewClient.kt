f4331014601698391a5c997ad4628e845b7ac7e3
Nuri Mertcan Guner <NMGuner@users.noreply.github.com>
2022-07-07 12:01:26 +0200

    add newer webview client method

--- android/src/main/kotlin/com/klarna/inapp/sdk/flutter_klarna_inapp_sdk/core/webview/KlarnaWebViewClient.kt
+++ android/src/main/kotlin/com/klarna/inapp/sdk/flutter_klarna_inapp_sdk/core/webview/KlarnaWebViewClient.kt
@@ -1,13 +1,25 @@
 package com.klarna.inapp.sdk.flutter_klarna_inapp_sdk.core.webview
 
+import android.annotation.TargetApi
+import android.os.Build
+import android.webkit.WebResourceRequest
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import com.klarna.mobile.sdk.api.hybrid.KlarnaHybridSDK
 
-internal open class KlarnaWebViewClient(private val klarnaHybridSDK: KlarnaHybridSDK) : WebViewClient() {
+internal open class KlarnaWebViewClient(private val klarnaHybridSDK: KlarnaHybridSDK) :
+    WebViewClient() {
 
     override fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean {
         return !klarnaHybridSDK.shouldFollowNavigation(url)
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest?): Boolean {
+        request?.url?.toString()?.let {
+            return !klarnaHybridSDK.shouldFollowNavigation(it)
+        }
+        return super.shouldOverrideUrlLoading(view, request)
     }
 
     override fun onPageFinished(view: WebView, url: String) {
