658af8ca7fc501d50a0061d965d7f8ea8734b4d4
Nuri Mertcan Guner <NMGuner@users.noreply.github.com>
2023-03-31 16:31:38 +0200

    remove unused native implementations

--- android/src/main/kotlin/com/klarna/inapp/sdk/flutter_klarna_inapp_sdk/ErrorCallbackHandler.kt
+++
@@ -1,8 +0,0 @@
-package com.klarna.inapp.sdk.flutter_klarna_inapp_sdk
-
-import com.klarna.inapp.sdk.flutter_klarna_inapp_sdk.core.stream.BaseStreamHandler
-
-// Usage:
-//ErrorCallbackHandler.sendValue("web view initialize failed")
-
-internal object ErrorCallbackHandler: BaseStreamHandler<String?>()
--- android/src/main/kotlin/com/klarna/inapp/sdk/flutter_klarna_inapp_sdk/ResultError.kt
+++ android/src/main/kotlin/com/klarna/inapp/sdk/flutter_klarna_inapp_sdk/ResultError.kt
@@ -3,8 +3,5 @@ package com.klarna.inapp.sdk.flutter_klarna_inapp_sdk
 internal enum class ResultError(val errorCode: String) {
     UNKNOWN_ERROR("UnknownError"),
     PLUGIN_METHOD_ERROR("KlarnaFlutterPluginMethodError"),
-    HYBRID_SDK_ERROR("KlarnaHybridSDKError"),
-    WEB_VIEW_ERROR("KlarnaWebViewError"),
-    POST_PURCHASE_ERROR("KlarnaPostPurchaseExperienceError"),
     KLARNA_POST_PURCHASE_SDK_ERROR("KlarnaPostPurchaseSDKError")
 }
