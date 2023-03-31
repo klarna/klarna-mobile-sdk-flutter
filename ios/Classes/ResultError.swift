47e12b0ba038639b0e3e609f510e7718a56f3b94
Nuri Mertcan Guner <NMGuner@users.noreply.github.com>
2023-03-31 16:40:15 +0200

    remove more unused ios native implementations

--- ios/Classes/ResultError.swift
+++ ios/Classes/ResultError.swift
@@ -1,8 +1,5 @@
 enum ResultError: String {
     case unknownError = "UnknownError"
     case pluginMethodError = "KlarnaFlutterPluginMethodError"
-    case hybridSdkError = "KlarnaHybridSDKError"
-    case webViewError = "KlarnaWebViewError"
-    case postPurchaseError = "KlarnaPostPurchaseExperienceError"
     case klarnaPostPurchaseSDKError = "KlarnaPostPurchaseSDKError"
 }
